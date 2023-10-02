f79840b2f2ca15b62c689baec94c3d589e8341ef
Garry <garrysjh@gmail.com>
2023-10-02 18:39:38 +0800

    remove diagrams

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/garrysjh/hello
+
+go 1.21.1
--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+	var whatToSay string = "Goodbye World!"
+	fmt.Println(whatToSay)
 }
